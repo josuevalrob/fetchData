5f80fcb484de2d278c3fc07385b749f2515beaba
JesusHuertas <jhuertascastellanos@hotmail.es>
2019-11-10 11:06:28 +0100

    refactor

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -1,120 +1,8 @@
 
 import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table } from 'react-virtualized';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import styles from './scoretable.styles'
-
-class MuiVirtualizedTable extends React.PureComponent {
-  static defaultProps = {
-    headerHeight: 48,
-    rowHeight: 48,
-  };
-
-  getRowClassName = ({ index }) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => {
-          return (
-          <Table
-            height={height}
-            width={width}
-            rowHeight={rowHeight}
-            gridStyle={{
-              direction: 'inherit',
-            }}
-            headerHeight={headerHeight}
-            className={classes.table}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={headerProps =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  width={width/columns.length}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      dataKey: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      numeric: PropTypes.bool,
-      width: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
-};
-
-const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
+import VirtualizedTable from './VirtualizedTable'
 
 export default function ReactVirtualizedTable({rows}) {
   return (
@@ -131,26 +19,22 @@ export default function ReactVirtualizedTable({rows}) {
             {
               label: 'First name',
               dataKey: 'first_name',
-              numeric: true,
             },
             {
               label: 'Gender',
               dataKey: 'gender',
-              numeric: true,
             },
             {
               label: 'City',
               dataKey: 'city',
-              numeric: true,
             },
             {
               label: 'Country',
               dataKey: 'country',
-              numeric: true,
             },
           ]}
           />
-        : <CircularProgress />
+        : <CircularProgress /> //💅
       }
     </Paper>
   );
--- src/scoretable.styles.js
+++ src/scoretable.styles.js
@@ -1,5 +1,3 @@
-const drawerWidth = 240;
-
 const styles = (theme) => ({
   flexContainer: {
     display: 'flex',
